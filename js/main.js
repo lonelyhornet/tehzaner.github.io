e70b8fbf291ce7284dd5e3ab2ddb2bf8fbed9539
TehZaner <tomstewart20@gmail.com>
2014-10-03 12:58:22 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
-var myName = "#Yolo";
+var myName = "Hi................";
 var letterColors = [red,orange,green,blue,purple];
 if(15 > 5) {
     bubbleShape = "circle";
